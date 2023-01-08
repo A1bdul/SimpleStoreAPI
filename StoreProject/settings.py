40edee93c9428376d7e922d70b8ea53c99ae24c3
divciprince <adaromosu@gmail.com>
2023-01-07 13:14:44 -1100

    csrf update domain

--- StoreProject/settings.py
+++ StoreProject/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+CSRF_TRUSTED_ORIGINS = ['https://simplestoreapi-production.up.railway.app','https://*.127.0.0.1']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
