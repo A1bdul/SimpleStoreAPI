0a51bfb5cbc847ce39a27a9a922d968414aaaa85
Abdulrahman Daromosu <73336016+A1bdul@users.noreply.github.com>
2023-01-08 19:12:06 +0100

    Update settings.py

--- StoreProject/settings.py
+++ StoreProject/settings.py
@@ -59,7 +59,8 @@ ROOT_URLCONF = 'StoreProject.urls'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8011',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'https://cs-electronics.up.railway.app'
 
 ]
 
