924fc432fe0f8bfca5700e9a7783b24f38954eeb
Abdulrahman Daromosu <73336016+A1bdul@users.noreply.github.com>
2023-01-10 18:12:43 +0100

    Update settings.py

--- StoreProject/settings.py
+++ StoreProject/settings.py
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'StoreProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': str(os.getenv("DB_NAME")),
-        'USER': str(os.getenv("DB_USER")),
-        'PASSWORD': str(os.getenv("DB_PASSWORD")),
-        'HOST': str(os.getenv("DB_HOST")),
-        'PORT': str(os.getenv("DB_PORT")),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': str(os.getenv("DB_NAME")),
+#        'USER': str(os.getenv("DB_USER")),
+#        'PASSWORD': str(os.getenv("DB_PASSWORD")),
+#        'HOST': str(os.getenv("DB_HOST")),
+#        'PORT': str(os.getenv("DB_PORT")),
+#    }
+#}
 
 
 # Password validation
