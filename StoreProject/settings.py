a4fb0678668efdce0ae7c8a91dd728141ce4673c
Abdulrahman Daromosu <73336016+A1bdul@users.noreply.github.com>
2023-01-10 16:01:01 +0100

    Update settings.py

--- StoreProject/settings.py
+++ StoreProject/settings.py
@@ -87,10 +87,22 @@ WSGI_APPLICATION = 'StoreProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Abduls', 
+        'USER': 'Abdul',
+        'PASSWORD': '123Abdul456',
+        'HOST': 'postgresql-104455-0.cloudclusters.net', 
+        'PORT': '10187',
     }
 }
 
